42e12c8dbb476978adb03c67d05236b291e0b1c6
RebecaNM30 <rebecanogueiramarinho@gmail.com>
2026-05-28 21:01:06 -0300

    Functions

--- biblioteca/math.js
+++ biblioteca/math.js
@@ -1,8 +1,17 @@
-function soma (z=w) {
-    let soma = z+w;
-    return soma;
-}
-const subtrair = function(z,w) {
-    return  z - w ;
-}
-module.exports = {soma,subtrair}
+function soma(z=2, w=4) { 
+    return z + w; 
+} 
+
+const subtrair = function(z=2, w=4) { 
+    return z - w; 
+}; 
+
+function multiplicacao(a=2, b=4) { 
+    return a * b; 
+} 
+
+const divisao = function(a=2, b=4) { 
+    return a / b; 
+};
+
+module.exports = { soma, subtrair, multiplicacao, divisao };

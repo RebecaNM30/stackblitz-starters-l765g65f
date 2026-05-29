803fd8871175b598e3c9a8a2c1994c23b6cdccd9
RebecaNM30 <rebecanogueiramarinho@gmail.com>
2026-05-28 21:20:54 -0300

    Functions

---
+++ usamath.js
@@ -0,0 +1,11 @@
+
+const oper = require('./biblioteca/definemodulo.js');
+
+var z = 2, w = 4;
+
+console.log(oper.soma(z, w));     
+console.log(oper.subtrair(z, w)); 
+console.log(oper.multicacao(a,b));
+console.log(oper.divisao(a,b))
+
+
